8ace2b7fbeafde66c03d1b6c79431c9e79c374c5
Fanuel Dana <fanueld@meta.com>
2024-06-26 11:19:48 -0700

    token not working

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const userRoutes = require("./routes/auth.js");
 var cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use("/login", userRoutes);
+app.use("/auth", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const { PrismaClient } = require("@prisma/client");
-
+require("dotenv").config();
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-
+//root route is /auth
 const prisma = new PrismaClient();
 // Route for user registration
 router.post("/users/signup", async (req, res) => {
-  const { username, password, email } = req.body;
-
   try {
-    const existingUser = await prisma.user.findFirst({
+    const { username, password, email } = req.body;
+    const existingUser = await prisma.users.findFirst({
       where: { OR: [{ username: username }, { email: email }] },
     });
 
@@ -23,14 +22,15 @@ router.post("/users/signup", async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await prisma.user.create({
+    const newUser = await prisma.users.create({
       data: {
         username: username,
         password: hashedPassword,
         email: email,
       },
     });
-    const token = jwt.sign({ newUser }, process.env.JWT_SECRET);
+
+    const token = jwt.sign(newUser, process.env.JWT_SECRET_KEY);
     res.json({ token });
   } catch (error) {
     console.error(error);
@@ -42,7 +42,7 @@ router.post("/users/login", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const user = await prisma.user.findUnique({ where: { username } });
+    const user = await prisma.users.findUnique({ where: { username } });
 
     if (!user) {
       return res.status(401).json({ error: "Invalid username or password" });
@@ -63,7 +63,7 @@ router.post("/users/login", async (req, res) => {
 router.get("/users/me", async (req, res) => {
   const token = req.headers.authorization;
 
-  const user = jwt.verify(token, process.env.JWT_SECRET);
+  const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
   res.send(user);
 });
 
